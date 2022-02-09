fc63224ff98e9771f43919fa165750e9f6ea9230
romanunr02 <romanunr02@gmail.com>
2022-02-09 23:59:19 +0200

    added scipt

--- scripts/main.js
+++ scripts/main.js
@@ -2,3 +2,11 @@
 var btn = new AboutDialog();
 btn.addCloseButton();
 new BaseDialog("base dialog")
+
+
+menuGroup.setFillParent(true);
+        menuGroup.touchable = Touchable.childrenOnly;
+menuGroup.visible(() => state.isMenu());
+        
+let menuF = new MenuFragment();
+menuF.build()
