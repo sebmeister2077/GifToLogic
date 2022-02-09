aefa1f8a9797302459137174397d54533cc9bef4
romanunr02 <romanunr02@gmail.com>
2022-02-10 00:00:10 +0200

    initialized menugroup

--- scripts/main.js
+++ scripts/main.js
@@ -3,10 +3,12 @@ var btn = new AboutDialog();
 btn.addCloseButton();
 new BaseDialog("base dialog")
 
+let menuGroup = new WidgetGroup();
 
 menuGroup.setFillParent(true);
-        menuGroup.touchable = Touchable.childrenOnly;
+menuGroup.touchable = Touchable.childrenOnly;
 menuGroup.visible(() => state.isMenu());
-        
+Core.scene.add(menuGroup);
+
 let menuF = new MenuFragment();
 menuF.build()
