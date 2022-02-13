c6bdb1b8362c91d41102d60de215685edc642313
romanunr02 <romanunr02@gmail.com>
2022-02-13 23:13:58 +0200

    test script

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,2 @@
-Vars.ui.showInfoPopup("Hello", 50000, 10, 10, 10, 10, 10);
-Vars.ui.showInfo("Hi5?")
-Vars.ui.showStartupInfo("Begin game")
-
-Vars.ui.showConfirm("confirm text", null)
+const about = Vars.ui.about;
+about.showCredits(); 
