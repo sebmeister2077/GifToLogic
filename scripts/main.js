026e00727e541a55b5def402f231da96aa18ec5a
romanunr02 <romanunr02@gmail.com>
2022-02-14 18:46:06 +0200

    test script

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,3 @@
 const ui = Vars.ui;
 
-let menuGroupaux = new WidgetGroup();
-menuGroupaux.setFillParent(true);
-menuGroupaux.touchable = Touchable.childrenOnly;
-menuGroupaux.visible= true;
-
-Core.scene.add(menuGroupaux)
-ui.menuFrag.build(menuGroupaux);
-
-ui.scriptfrag.addMessage("Hi from console?")
+Log.info("hi from terminal")
