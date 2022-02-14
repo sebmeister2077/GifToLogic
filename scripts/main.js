b01325c62aa3ef96e3f55e521b42d2e24290e19c
romanunr02 <romanunr02@gmail.com>
2022-02-14 20:06:51 +0200

    added test dialog

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,12 @@
 
 Log.info("hi from terminal")
-Vars.ui.aboutDialog = null;
+
+const dialog = new BaseDialog(title);
+        dialog.cont.add(text).width(500).wrap().pad(4).get().setAlignment(Align.center, Align.center);
+        dialog.buttons.defaults().size(200, 54).pad(2);
+        dialog.setFillParent(false);
+        dialog.buttons.button("@ok", () => {
+            dialog.hide();
+            confirmed.run();
+        });
+        dialog.show();
