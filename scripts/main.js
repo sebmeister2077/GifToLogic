fc0572b6a61f31bddbe9f09830e6fc3a0e31c3ec
romanunr02 <romanunr02@gmail.com>
2022-03-29 23:11:08 +0300

    use custom style

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,12 @@
 Log.info("hi from terminal");
-Log.info(Styles)
-const dialog = new BaseDialog("some title", Styles.defaultDialog);
+
+const dialogStyle = new DialogStyle();
+dialogStyle.titleFont = Fonts.def;
+dialogStyle.background = windowEmpty;
+dialogStyle.titleFontColor = Pal.accent;
+dialogStyle.stageBackground = black9;
+
+const dialog = new BaseDialog("some title", dialogStyle);
 dialog.cont.add("some text idk").width(500).wrap().pad(4).get().setAlignment(Align.center, Align.center);
 dialog.buttons.defaults().size(200, 54).pad(2);
 dialog.setFillParent(false);
