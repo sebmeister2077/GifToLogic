c21b40124eb6ffc63e97371e7b4a1acea92c8f12
romanunr02 <romanunr02@gmail.com>
2022-03-29 22:56:17 +0300

    fixed error

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,12 @@
+Log.info("hi from terminal");
+Log.info(Fonts)
 
-Log.info("hi from terminal")
-
-const dialog = new BaseDialog("some title");
-        dialog.cont.add("some text idk").width(500).wrap().pad(4).get().setAlignment(Align.center, Align.center);
-        dialog.buttons.defaults().size(200, 54).pad(2);
-        dialog.setFillParent(false);
-        dialog.buttons.button("@ok", () => {
-            dialog.hide();
-            confirmed.run();
-        });
-        dialog.show();
+const dialog = new BaseDialog("some title", defaultDialog);
+dialog.cont.add("some text idk").width(500).wrap().pad(4).get().setAlignment(Align.center, Align.center);
+dialog.buttons.defaults().size(200, 54).pad(2);
+dialog.setFillParent(false);
+dialog.buttons.button("@ok", () => {
+	dialog.hide();
+	confirmed.run();
+});
+dialog.show();
