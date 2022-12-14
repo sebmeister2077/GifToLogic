ec2ce361ba5f2a0727100e8da62435cf3d5b0799
romanunr02 <romanunr02@gmail.com>
2022-12-14 20:56:33 +0200

    test out toast message

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,11 @@ myDialog.addCloseButton();
 myDialog.cont.add("Goodbye.");
 // Show dialog
 myDialog.show();
+
+// listen for the event where a unit is destroyed
+Events.on(UnitDestroyEvent, (event) => {
+	// display toast on top of screen when the unit was a player
+	if (event.unit.isPlayer()) {
+		Vars.ui.hudfrag.showToast("Pathetic.");
+	}
+});
