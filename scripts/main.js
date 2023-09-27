047a49d30ed117151d1e7298cf6cb76502d1d51b
romanunr02 <romanunr02@gmail.com>
2023-09-27 23:04:04 +0300

    test documentation example

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,16 @@
-Log.info("hi from terminal");
+// Log.info("hi from terminal");
 
-// todo view https://vscode.dev/github/Anuken/Arc/blob/master/arc-core/src/arc/scene/ui/Dialog.java
-const myDialog = new BaseDialog("Dialog Title");
-// Add "go back" button
-myDialog.addCloseButton();
-// Add text to the main content
-myDialog.cont.add("Goodbye.");
-// Show dialog
-myDialog.show();
+// // todo view https://vscode.dev/github/Anuken/Arc/blob/master/arc-core/src/arc/scene/ui/Dialog.java
+// const myDialog = new BaseDialog("Dialog Title");
+// // Add "go back" button
+// myDialog.addCloseButton();
+// // Add text to the main content
+// myDialog.cont.add("Goodbye.");
+// // Show dialog
+// myDialog.show();
 
 // listen for the event where a unit is destroyed
 Events.on(UnitDestroyEvent, (event) => {
 	// display toast on top of screen when the unit was a player
-	if (event.unit.isPlayer()) {
-		Vars.ui.hudfrag.showToast("Pathetic.");
-	}
+	Vars.ui.hudfrag.showToast("Pathetic.");
 });
