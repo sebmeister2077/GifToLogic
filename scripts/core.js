0e549e4063b8bcc1d99f3a120b728889fd1e7b4a
romanunr02 <romanunr02@gmail.com>
2023-09-30 22:22:39 +0300

    test more code

---
+++ scripts/core.js
@@ -0,0 +1,5 @@
+const core = {
+	gif: null,
+};
+
+module.exports = core;
--- scripts/main.js
+++ scripts/main.js
@@ -1,70 +1,43 @@
 const ui = require("ui_lib/library");
+const coreLogic = require("gittologic/core");
+
 var ptl = null;
-ui.addMenuButton("testbtn", "paste", () => {
+
+const buttonNames = "Gif to Logic";
+ui.addMenuButton(buttonNames, Icon.paste, () => {
 	ptl.show();
 });
 
 ui.onLoad(() => {
 	// Add button in Schematics dialog
-	Vars.ui.schematics.buttons.button("testbtn", Icon.paste, () => {
+	Vars.ui.schematics.buttons.button(buttonNames, Icon.paste, () => {
 		ptl.show();
 	});
 
-	ptl = new BaseDialog("testbtn");
+	ptl = new BaseDialog(buttonNames);
 
-	print("hello");
-
-	ptl.cont.add("[coral]1.[] Select a PNG image.");
+	ptl.cont.add("[coral]1.[] Select a GIF(2MB).");
 	ptl.cont.row();
 	ptl.cont.add("[coral]2.[] Click [stat]Export[] to create a schematic.");
 	ptl.cont.row();
-	ptl.cont.add("[coral]Please dont use this for furry/weeb shit thank you");
-	ptl.cont.row();
 
 	ptl.cont
-		.button("Select Image", () => {
-			Vars.platform.showFileChooser(false, "png", (file) => {
+		.button("Select Gif", () => {
+			Vars.platform.showFileChooser(false, "gif", (file) => {
 				try {
+					print(file);
 					const bytes = file.readBytes();
-					core.image = new Pixmap(bytes);
+					print(bytes);
+					// core.image = new Pixmap(bytes);
 				} catch (e) {
-					ui.showError("Failed to load source image", e);
+					ui.showError("Failed to load source gif", e);
 				}
 			});
 		})
 		.size(240, 50);
-	ptl.cont.row();
 
+	ptl.cont.row();
 	ptl.cont.label(() => "This is label...").center();
 
 	ptl.addCloseButton();
 });
-
-// Log.info("hi from terminal");
-
-// // todo view https://vscode.dev/github/Anuken/Arc/blob/master/arc-core/src/arc/scene/ui/Dialog.java
-// const myDialog = new BaseDialog("Dialog Title");
-// // Add "go back" button
-// myDialog.addCloseButton();
-// // Add text to the main content
-// myDialog.cont.add("Goodbye.");
-// // Show dialog
-// myDialog.show();
-
-// listen for the event where a unit is destroyed
-// Events.on(ClientCreateEvent, (event) => {
-// 	// display toast on top of screen when the unit was a player
-// 	Vars.ui.hudfrag.showToast("client create");
-// });
-// //this make the app crash
-// // Events.on(ContentInitEvent, (event) => {
-// // 	// display toast on top of screen when the unit was a player
-// // 	setTimeout(() => {
-// // 		Vars.ui.hudfrag.showToast("content init");
-// // 	}, 2000);
-// // });
-
-// Events.on(ClientLoadEvent, (event) => {
-// 	// display toast on top of screen when the unit was a player
-// 	Vars.ui.hudfrag.showToast("client load");
-// });
