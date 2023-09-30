b2b23fd832aef3d1f65bf81a9adb87eab682f331
romanunr02 <romanunr02@gmail.com>
2023-10-01 00:39:57 +0300

    prepare base code for the UI

--- scripts/core.js
+++ scripts/core.js
@@ -1,5 +1,16 @@
 const core = {
-	gif: null,
+	//this is the base display
+	display: Blocks.logicDisplay,
+	size: Blocks.logicDisplay.displaySize,
+
+	gifbytes: null,
+
+	isWorking: false,
+	workingStateLabel: "",
+};
+
+core.exportGif = (gifBytes) => {
+	print("Finish");
 };
 
 module.exports = core;
--- scripts/main.js
+++ scripts/main.js
@@ -1,37 +1,31 @@
 const ui = require("ui_lib/library");
 const coreLogic = require("giftologic/core");
 
-var ptl = null;
-
-const buttonNames = "Gif to Logic";
-ui.addMenuButton(buttonNames, "paste", () => {
-	ptl.show();
-});
-
 ui.onLoad(() => {
-	ui.addMenuButton(buttonNames, Icon.paste, () => {});
+	const buttonNames = "Gif to Logic";
+	let dialog = null;
+	ui.addMenuButton(buttonNames, Icon.paste, () => {
+		dialog;
+	});
 	// Add button in Schematics dialog
 	Vars.ui.schematics.buttons.button(buttonNames, Icon.paste, () => {
-		ptl.show();
+		dialog.show();
 	});
 
-	ptl = new BaseDialog(buttonNames);
+	dialog = new BaseDialog(buttonNames);
 
-	ptl.cont.add("[coral]1.[] Select a GIF(2MB).");
-	ptl.cont.row();
-	ptl.cont.add("[coral]2.[] Click [stat]Export[] to create a schematic.");
-	ptl.cont.row();
+	dialog.cont.add("[coral]1.[] Select a GIF(2MB).");
+	dialog.cont.row();
+	dialog.cont.add("[coral]2.[] Click [stat]Export[] to create a schematic.");
+	dialog.cont.row();
 
-	ptl.cont
+	dialog.cont
 		.button("Select Gif", () => {
 			Vars.platform.showFileChooser(false, "gif", (file) => {
 				try {
-					print(file);
 					const bytes = file.readBytes();
-					print(bytes.length);
-					print(bytes[0]);
-					print(bytes[1]);
-					// core.image = new Pixmap(bytes);
+					coreLogic.gifbytes = bytes;
+					// coreLogic.image = new Pixmap(bytes);
 				} catch (e) {
 					ui.showError("Failed to load source gif", e);
 				}
@@ -39,8 +33,23 @@ ui.onLoad(() => {
 		})
 		.size(240, 50);
 
-	ptl.cont.row();
-	ptl.cont.label(() => "This is label...").center();
+	dialog.cont.row();
+	dialog.cont.label(() => coreLogic.workingStateLabel).center();
+	dialog.buttons
+		.button("Export", Icons.export, () => {
+			new java.lang.Thread(() => {
+				try {
+					coreLogic.exportGif(coreLogic.gifbytes);
+					dialog.hide();
+				} catch (e) {
+					Core.app.post(() => {
+						ui.showError("Failed to export schematic", e);
+						coreLogic.isWorking = false;
+					});
+				}
+			}, "GifToLogic worker").start();
+		})
+		.disabled(() => !coreLogic.gifbytes || !coreLogic.isWorking);
 
-	ptl.addCloseButton();
+	dialog.addCloseButton();
 });
