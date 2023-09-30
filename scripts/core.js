c40e63730b9249dcb16d31cd23ec9e0c62715f56
romanunr02 <romanunr02@gmail.com>
2023-10-01 00:50:03 +0300

    test out if i can use any js package

--- scripts/core.js
+++ scripts/core.js
@@ -1,3 +1,6 @@
+const fs = require("fs");
+const gif = require("gif");
+
 const core = {
 	//this is the base display
 	display: Blocks.logicDisplay,
@@ -10,6 +13,7 @@ const core = {
 };
 
 core.exportGif = (gifBytes) => {
+	print(typeof fs.readFileSync);
 	print("Finish");
 };
 
